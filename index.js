2dad37f72be61328bdb2927296253819fe867e34
Lungile (Lulu) Madondo <125693019+MadondoL@users.noreply.github.com>
2023-05-13 13:58:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function updateWeather(response) {
 function getForecast(coordinates) {
   let lat = coordinates.lat;
   let lon = coordinates.lon;
-  let apiKey = "e0993610d858f5529b5b4f6a32da6070";
+  let apiKey ="e0993610d858f5529b5b4f6a32da6070"; 
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
