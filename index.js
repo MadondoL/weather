5083577b0cb76b892ff7d1c55219b3340311461a
Lungile (Lulu) Madondo <125693019+MadondoL@users.noreply.github.com>
2023-05-13 12:46:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -147,3 +147,4 @@ form.addEventListener("submit", handleFormSubmit);
 // Add event listener to the "current" button
 let button = document.querySelector("#current");
 button.addEventListener("click", handleCurrentButtonClick);
+displayForecast()
