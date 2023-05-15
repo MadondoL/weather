cd5cd348f1f5d0836a910f644c5debf38bec7d04
Lungile (Lulu) Madondo <125693019+MadondoL@users.noreply.github.com>
2023-05-15 21:19:25 +0200

    Show one city at all times

--- index.js
+++ index.js
@@ -178,3 +178,4 @@ form.addEventListener("submit", handleFormSubmit);
 // Add event listener to the "current" button
 let button = document.querySelector("#current");
 button.addEventListener("click", handleCurrentButtonClick);
+getWeatherByCity("Johannesburg");
